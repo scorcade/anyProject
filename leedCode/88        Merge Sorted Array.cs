a17d0cf3cea59e1b1e16a1d255ae05b587b9a56e
scorcade <scorcade@outlook.com>
2023-06-10 17:40:21 +0800

    88 Merge_Sorted_Array

--- leedCode/88        Merge Sorted Array.cs
+++ leedCode/88        Merge Sorted Array.cs
@@ -15,7 +15,20 @@ namespace leedCode88
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
+            int pM = m - 1;
+            int pN = n-1;
+            int PNM = n+m-1;
+            int cur=0;
 
+            while(pN > 0 || pM > 0)
+            {
+                if (pM == -1) { cur = nums2[pN--]; }
+                else if (pN==-1){ cur = nums1[pM--]; }
+                else if (nums1[pM] > nums2[pN]) { cur = nums1[pM--]; }
+                else { cur = nums2[pN--]; }
+              
+                nums1[PNM--] = cur;
+            }
         }
     }
 }
